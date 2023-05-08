62c838dfdaf31ce9630bb6e849de879753893c44
telemc97 <telemc97@gmail.com>
2023-05-08 23:50:21 +0300

    Working TODO: Fix displaying add data structs & 3D

--- include/ConvexHull2D.cpp
+++ include/ConvexHull2D.cpp
@@ -9,41 +9,43 @@ ConvexHull2D::ConvexHull2D(vector<Point2D>& points){
   }
 }
 
+//0:CW, 1:CCW, 2:Coolinear 
 int ConvexHull2D::pointsOrientation(Point2D& pointA, Point2D& pointB, Point2D& pointC){
   Eigen::Matrix3i detMatrix;
-  detMatrix << 1, pointA.x, pointA.y, 1, pointB.x, pointB.y, 1, pointC.x, pointC.y;
-  double determ = detMatrix.determinant();
+  detMatrix <<1, pointA.x, pointA.y, 1, pointB.x, pointB.y, 1, pointC.x, pointC.y;
+  int det_eval = (pointB.y - pointA.y) * (pointC.x - pointB.x) - (pointB.x - pointA.x) * (pointC.y - pointB.y);
+  int determ = detMatrix.determinant();
   if (determ<0){
-    return 0; //CW
-  }else if(determ>0){
-    return 1; //CCW
+    return 0;
+  }else if (determ>0){
+    return 1;
   }else{
-    return 2; //Coolinear
+    return 2;
   }
 }
 
 void ConvexHull2D::jarvisAlgorithm(){
   // mainSortingSingleThread(_points, 0, array_size-1);
   int l=0;
-  for (int i=0;i<_points.size();i++){
+  for (int i=1;i<_points.size();i++){
     if(_points[i].x<_points[l].x){
       l=i;
     }
   }
-  //the leftmost point is the first point after mergesort l=0 & q=0.
-  int p=l,q;
+  int p=l;
+  int q;
   do{
     _hull_points.push_back(_points[p]);
     q=(p+1)%_points.size();
     for (int i=0;i<_points.size();i++){
-      if (pointsOrientation(_points[p], _points[i], _points[q])==1){
+      if (pointsOrientation(_points[p], _points[i], _points[q])==0){
         q=i;
       }
-      p=q;
     }
+    p=q;
   }while(p!=l);
 }
 
-vector<Point2D> ConvexHull2D::getHullPoints(){
+vector<Point2D> ConvexHull2D::getHullLines(){
   return _hull_points;
 }
